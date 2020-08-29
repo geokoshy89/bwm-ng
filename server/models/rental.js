30c22db63535818d9ac10d75fe88a0341836f019
George Koshy <geo.koshy89@gmail.com>
2020-08-29 22:19:45 +0530

    Add basic server setup

---
+++ server/models/rental.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const rentalSchema=new Schema({
+  title:{type:String,required:true,max:[120,'Too lone, max is 120 characters']},
+  city:{type:String,required:true,lowerCase:true},
+  street:{type:String,required:true,min:[4,'Too short,min is 4 characters']},
+  category:{type:String,required:true,lowerCase:true},
+  image:{type:String,required:true},
+  bedrooms:Number,
+  shared:Boolean,
+  description:{type:String,required:true},
+  dailyRate:Number,
+  createdAt:{type:Date,default:Date.now}
+});
+
+module.exports=mongoose.model('Rental',rentalSchema);
