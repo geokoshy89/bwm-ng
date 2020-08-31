a3b12f47e3778232d7d7e2d9cf43dc91243b4cc3
George Koshy <geo.koshy89@gmail.com>
2020-09-01 01:25:00 +0530

    Add register ,adds login and auth middleware routes on the server

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const router=express.Router();
 const Rental=require('../models/rental');
+const UserCtrl=require('../controllers/user');
+router.get('/secret',UserCtrl.authMiddleware,function(req,res){
+  res.json({"secret":true});
+});
 router.get('',function(req,res){
   Rental.find({},function(err,foundRentals){
     res.json(foundRentals);
-  })
-})
+  });
+});
 
 router.get('/:id',function(req,res){
   const rentalId=req.params.id;
